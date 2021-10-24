06ef5c0b966f39a39d55d0a68f4eee80834567f7
DripTill <91988627+DripTill@users.noreply.github.com>
2021-10-23 23:33:32 -0500

    Add files via upload

---
+++ MOD 4 TEAMWORK.py
@@ -0,0 +1,11 @@
+print("1. Learn Python")
+print("2. Learn Java")
+print("3. Go Swimming")
+print("4. Have Dinner")
+print("5. Go To Bed")
+print("0. Exit")
+
+question = input("what is ur num?")
+    if question is 1:
+
+
